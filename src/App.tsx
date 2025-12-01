8c4e9e42dcf65313a73aec573cb11b8454491441
Eliam28 <152578969+Eliam28@users.noreply.github.com>
2025-11-30 19:57:00 -0600

    varios pokemones

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,32 @@ import "./App.css";
 import PokemonCard from "./Componentes/PokemonCard";
 import type { PokemonApi } from "./PokemonApi";
 function App() {
-  const [pokemonInfo, setPokemonInfo] = useState<PokemonApi | null>(null);
+  const [pokemonInfo, setPokemonInfo] = useState<PokemonApi[]>([]);
   const URL_BASE = "https://pokeapi.co/api/v2/pokemon/";
 
   async function busquedaPokemon(id: number) {
     try {
       const respuesta = await fetch(`${URL_BASE}${id}`);
       const data: PokemonApi = await respuesta.json();
-      console.log(data);
-      setPokemonInfo(data);
+      setPokemonInfo((pokeActu) => {
+        const copiaPokeInfo = [...pokeActu];
+        copiaPokeInfo.push(data);
+        return copiaPokeInfo;
+      });
     } catch (error) {
       console.error("Error con la respuesta al buscar la info", error);
     }
   }
 
+  function ObtenerTodosLosPokemos() {
+    for (let i = 0; i < 20; i++) {
+      busquedaPokemon(i);
+    }
+  }
+
   useEffect(() => {
     const cargar = async () => {
-      await busquedaPokemon(1);
+      await ObtenerTodosLosPokemos();
     };
     cargar();
   }, []);
@@ -29,9 +38,9 @@ function App() {
       <h1>Pokedex</h1>
       <div className="ListaDePokemon">
         <div className="PokemonCards">
-          <PokemonCard pokemonData={pokemonInfo} />
-          <PokemonCard pokemonData={pokemonInfo} />
-          <PokemonCard pokemonData={pokemonInfo} />
+          {pokemonInfo.map((pokemon, index) => (
+            <PokemonCard key={index} pokemonData={pokemon} />
+          ))}
         </div>
       </div>
     </>
